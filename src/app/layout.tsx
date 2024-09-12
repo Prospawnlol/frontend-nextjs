2bd3f4ff17b253c7daeca05507b7955a6afc214a
Prospawnlol <pablojaredmosquera@gmail.com>
2024-09-12 13:43:59 -0500

    Actualización de la estructura del proyecto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "../app/Navbar/Navbar"
 import Sidebar from "../app/Sidebar/Sidebar";
 
 const geistSans = localFont({
